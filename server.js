e6150f4ec8c31134d1528fa32f51621886eb29d6
freetrde7 <freetrde7@gmail.com>
2021-02-17 09:52:17 +0700

    should get users from array but does not

--- server.js
+++ server.js
@@ -5,22 +5,28 @@ const express = require('express');
 const app = express();
 
 //mock user data:
+
 const mockUserData=[
-  {name: 'Mark'},
-  {name: 'Jill'}
+	{name:'Mark'},
+	{name:'Jill'}
 ]
 
-app.get('/users', function(req,res){
-  res.json({
-    success: true,
-    message: 'successfully got users!',
-    users: mockUserData
-  })
+app.get('/users',function(req,res){
+	res.json({
+		success: true,
+		message: 'successfully got users. Nice!',
+		users: mockUserData
+	})
 })
-//code here
 
-//bottom
-app.listen(8000, function(){
-  console.log("Server is listening")
-}
-  )
+app.get('/users/:id',function(req,res){
+	console.log(req.params.id)
+	res.json({
+		success: true,
+		message: 'got one user',
+		user: req.params.id
+	})
+})
+
+
+app.listen(8000,function(){console.log('server is listening')})
