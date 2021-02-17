165ed523715d2684a77ca9129d2639b82aad19e9
freetrde7 <freetrde7@gmail.com>
2021-02-17 10:07:18 +0700

    commiting username/psw functionality

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 //gives access to express librrary by searching node_modules and finding "express"
 const express = require('express');
-
-//creates an instance of express constructor named "app"
 const app = express();
+const bodyParser = require('body-parser');
 
-//mock user data:
+app.use(bodyParser.json())
 
 const mockUserData=[
 	{name:'Mark'},
@@ -18,7 +17,7 @@ app.get('/users',function(req,res){
 		users: mockUserData
 	})
 })
-
+// colons are used as variables that be viewed in the params
 app.get('/users/:id',function(req,res){
 	console.log(req.params.id)
 	res.json({
@@ -28,5 +27,29 @@ app.get('/users/:id',function(req,res){
 	})
 })
 
+app.post('/login',function(req,res){
+	// Typically passwords are encrypted using something like bcrypt before sending to database
+	const username=req.body.username;
+	const password=req.body.password;
+
+	// This should come from the database
+	const mockUsername="billyTheKid";
+	const mockPassword="superSecret";
+
+	if (username===mockUsername && password===mockPassword){
+		// In practice, use JSON web token sign method here to make an encrypted token
+		res.json({
+			success: true,
+			message: 'password and username match!',
+			token: 'encrypted token goes here'
+		})
+	} else {
+		res.json({
+			success: false,
+			message: 'password and username do not match'
+		})
+	}
+
+})
 
 app.listen(8000,function(){console.log('server is listening')})
